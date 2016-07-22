ea6c3449485feed2b7ee808a710471633bf73d44
Todd D. Vance <tdvance@gmail.com>
2016-07-22 15:59:48 -0400

    Projectile class

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -20,6 +20,10 @@ void ATank::SetTurretReference(UTankTurret* TurretToSet) {
 	TankAimingComponent->SetTurretReference(TurretToSet);
 }
 
+void ATank::Fire() {
+	UE_LOG(LogTemp, Warning, TEXT("Fire!"));
+}
+
 
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -24,6 +24,9 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void SetTurretReference(UTankTurret* TurretToSet);
 
+	UFUNCTION(BlueprintCallable, Category = Firing)
+		void Fire();
+
 
 private:
 	// Sets default values for this pawn's properties
